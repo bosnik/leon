ea346e38f1c5bedf3d4a7c4549644e54a0899bb1
Rodion Voskoboinik <rodion1991313@gmail.com>
2024-04-18 23:15:13 +0200

    fix slider-mini

--- dist/script.js
+++ dist/script.js
@@ -148,52 +148,47 @@ class MiniSlider extends _slider__WEBPACK_IMPORTED_MODULE_0__["default"] {
     }
   }
   bindTriggers() {
-    this.next.addEventListener('click', () => {
-      this.container.appendChild(this.slides[0]);
-      this.slides = [...this.container.children];
-      // this.resetActicveBtn();      
-      this.decorizeSlides();
-    });
+    this.nextSLide();
     this.prev.addEventListener('click', () => {
       let active = this.slides[this.slides.length - 1];
       this.container.insertBefore(active, this.slides[0]);
       this.slides = [...this.container.children];
-      // this.resetActicveBtn();
+      this.resetActicveBtn();
       this.decorizeSlides();
     });
   }
+  resetActicveBtn() {
+    // Sprawdź, czy kontener pasuje do selektora '.feed__slider'
+    if (this.container.classList.contains('feed__slider')) {
+      // Znajdź kontener o nazwie 'feed__slider'
+      const container = document.querySelector('.feed__slider');
 
-  // resetActicveBtn() {
-  //     const container = document.querySelector('.feed__slider');
-
-  //     // Sprawdź, czy kontener istnieje
-  //     if (container) {
-  //         // Jeśli znaleziono przyciski, wykonaj operacje
-  //         if (this.next && this.prev) {
-  //             // Przesuń przyciski next i prev na środek kontenera feed__slider
-  //             const middleIndex = Math.floor(this.slides.length / 2);
-  //             container.insertBefore(this.next, this.slides[middleIndex]);
-  //             container.insertBefore(this.prev, this.slides[middleIndex]);
-  //             // Zaktualizuj listę slajdów
-  //             this.slides = [...container.children];
-  //         } else {
-  //             console.error("Nie znaleziono przycisków 'next' i 'prev' w kontenerze 'feed__slider'");
-  //         }
-  //     } else {
-  //         console.error("Nie znaleziono kontenera 'feed__slider'");
-  //     }
-
-  // }
-
-  // nextSLide() {
-  //     this.next.addEventListener('click', () => {
-  //         this.resetActicveBtn();
-  //         this.container.appendChild(this.slides[0]);
-  //         this.slides = [...this.container.children];          
-  //         this.decorizeSlides();
-  //     });
-  // }
-
+      // Sprawdź, czy kontener istnieje
+      if (container) {
+        // Jeśli znaleziono przyciski, wykonaj operacje
+        if (this.next && this.prev) {
+          // Przesuń przyciski next i prev na środek kontenera feed__slider
+          const middleIndex = Math.floor(this.slides.length / 2);
+          container.insertBefore(this.next, this.slides[middleIndex]);
+          container.insertBefore(this.prev, this.slides[middleIndex]);
+          // Zaktualizuj listę slajdów
+          this.slides = [...container.children];
+        } else {
+          console.error("Nie znaleziono przycisków 'next' i 'prev' w kontenerze 'feed__slider'");
+        }
+      } else {
+        console.error("Nie znaleziono kontenera 'feed__slider'");
+      }
+    }
+  }
+  nextSLide() {
+    this.next.addEventListener('click', () => {
+      this.container.appendChild(this.slides[0]);
+      this.slides = [...this.container.children];
+      this.resetActicveBtn();
+      this.decorizeSlides();
+    });
+  }
   init() {
     this.container.style.cssText = `
         display: flex;
